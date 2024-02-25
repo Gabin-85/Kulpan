15e39d6f89341d256cb6b6adb3df6db284251329
Gabin-85 <quagligabin.praud@gmail.com>
2024-02-25 19:25:49 +0100

    Assertion error and logger

--- testbed/scr/main.c
+++ testbed/scr/main.c
@@ -1,6 +1,15 @@
-#include "../../engine/scr/test.h"
+#include "../../engine/scr/core/logger.h"
+#include "../../engine/scr/core/asserts.h"
 
 int main(void) {
-    print_int(42);
+    KFATAL("A test message: %f", 3.14f);
+    KERROR("A test message: %f", 3.14f);
+    KWARN("A test message: %f", 3.14f);
+    KINFO("A test message: %f", 3.14f);
+    KDEBUG("A test message: %f", 3.14f);
+    KTRACE("A test message: %f", 3.14f);
+
+    KASSERT(1 == 0);
+
     return 0;
 }
