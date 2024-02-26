201b80cfb762840cd0f3a2088b0e014ca6b8a93a
Gabin-85 <quagligabin.praud@gmail.com>
2024-02-26 11:30:42 +0100

    Application layer and entry points (tested)

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ It is linux and windows compactible.
 
 ## How do you run it ?
 
-To run it just open it with VSCode,
+To run it just open it with VSCode, 
 press Ctrl+Shift+B to build it and 
 press F5 to run the testbed.
--- engine/scr/platform/platform.h
+++ engine/scr/platform/platform.h
@@ -6,7 +6,7 @@ typedef struct platform_state {
     void* internal_state;
 } plateform_state;
 
-KAPI b8 platform_startup(
+b8 platform_startup(
     plateform_state* plat_state,
     const char* application_name,
     i32 x,
@@ -15,12 +15,12 @@ KAPI b8 platform_startup(
     i32 height
 );
 
-KAPI void platform_shutdown(struct platform_state* plat_state);
+void platform_shutdown(struct platform_state* plat_state);
 
-KAPI b8 platform_pump_messages(struct platform_state* plat_state);
+b8 platform_pump_messages(struct platform_state* plat_state);
 
-void* platform_allocate(u64 size, b8 aligned);
-void platform_free(void* block, b8 aligned);
+KAPI void* platform_allocate(u64 size, b8 aligned);
+KAPI void platform_free(void* block, b8 aligned);
 void* platform_zero_memory(void* block, u64 size);
 void* platform_copy_memory(void* dest, const void* source, u64 size);
 void* platform_set_memory(void* dest, i32 value, u64 size);
