ec0a421ca4148c1c341b37400868c3c669fbbf8a
Gabin-85 <quagligabin.praud@gmail.com>
2024-02-28 21:08:18 +0100

    Big custom math lib "\_(°-°)_/"

---
+++ engine/src/math/math_types.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include "defines.h"
+
+typedef union vec2_u {
+    //An array of x, y
+    f32 elements[2];
+    struct {
+        union {
+            //The first element
+            f32 x, r, s, u;
+        };
+        union {
+            //The second element
+            f32 y, g, t, v;
+        };
+    };
+} vec2;
+
+typedef struct vec3_u {
+    union {
+        //An array of x, y, z
+        f32 elements[3];
+        struct {
+            union {
+                //The first element
+                f32 x, r, s, u;   
+            };
+            union {
+                //The second element
+                f32 y, g, t, v;
+            };
+            union {
+                //The third element
+                f32 z, b, p, w;
+            };
+        };
+    };
+} vec3;
+
+typedef union vec4_u {
+    //An array of w, x, y, z
+    f32 elements[4];
+    union {
+        struct {
+            union {
+                // The first element
+                f32 x, r, s;
+            };
+            union {
+                // The second element
+                f32 y, g, t;
+            };
+            union {
+                // The third element
+                f32 z, b, p;
+            };
+            union {
+                // The fourth element
+                f32 w, a, q;
+            };
+        };
+    };
+} vec4;
+
+typedef vec4 quat;
+
+typedef union mat4_u {
+    f32 data[16];
+} mat4;
