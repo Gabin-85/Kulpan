2d038fe9c3e6bb6cfa12b0f7238b95574e421282
Gabin-85 <quagligabin.praud@gmail.com>
2024-02-28 18:34:35 +0100

    Make build system

--- build-all.bat
+++ build-all.bat
@@ -1,16 +1,24 @@
 @ECHO OFF
 REM Build Everything
 
-ECHO "Building everything..."
+@rem ECHO "Building everything..."
 
-PUSHD engine
-CALL build.bat
-POPD
+@rem PUSHD engine
+@rem CALL build.bat
+@rem POPD
+@rem IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
+
+@rem PUSHD testbed
+@rem CALL build.bat
+@rem POPD
+@rem IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
+
+REM Engine
+make -f "Makefile.engine.windows.mak" all
 IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
 
-PUSHD testbed
-CALL build.bat
-POPD
+REM Testbed
+make -f "Makefile.testbed.windows.mak" all
 IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
 
 ECHO "All assemblies built successfully."
