12c6fe403f40bc99ca7469e38bddc76821142df4
Gabin-85 <quagligabin.praud@gmail.com>
2024-03-24 13:03:22 +0100

    Writetable textures and a bit of refactoring

--- engine/src/systems/texture_system.h
+++ engine/src/systems/texture_system.h
@@ -66,6 +66,20 @@ void texture_system_shutdown(void* state);
 texture* texture_system_acquire(const char* name, b8 auto_release);
 
 /**
+ * @brief Attempts to acquire a writeable texture with the given name. This does not point to
+ * nor attempt to load a texture file. Does also increment the reference counter.
+ * NOTE: Writeable textures are not auto-released.
+ *
+ * @param name The name of the texture to acquire.
+ * @param width The texture width in pixels.
+ * @param height The texture height in pixels.
+ * @param channel_count The number of channels in the texture (typically 4 for RGBA)
+ * @param has_transparency Indicates if the texture will have transparency.
+ * @return A pointer to the generated texture.
+ */
+texture* texture_system_aquire_writeable(const char* name, u32 width, u32 height, u8 channel_count, b8 has_transparency);
+
+/**
  * @brief Releases a texture with the given name. Ignores non-existant textures.
  * Decreases the reference counter by 1. If the reference counter reaches 0 and
  * auto_release was set to true, the texture is unloaded, releasing internal resources.
@@ -73,6 +87,59 @@ texture* texture_system_acquire(const char* name, b8 auto_release);
  * @param name The name of the texture to unload.
  */
 void texture_system_release(const char* name);
+
+/**
+ * @brief Wraps the provided internal data in a texture structure using the parameters
+ * provided. This is best used for when the renderer system creates internal resources
+ * and they should be passed off to the texture system. Can be looked up by name via
+ * the acquire methods.
+ * NOTE: Wrapped textures are not auto-released.
+ *
+ * @param name The name of the texture.
+ * @param width The texture width in pixels.
+ * @param height The texture height in pixels.
+ * @param channel_count The number of channels in the texture (typically 4 for RGBA)
+ * @param has_transparency Indicates if the texture will have transparency.
+ * @param is_writeable Indicates if the texture is writeable.
+ * @param internal_data A pointer to the internal data to be set on the texture.
+ * @param register_texture Indicates if the texture should be registered with the system.
+ * @return A pointer to the wrapped texture.
+*/
+texture* texture_system_wrap_internal(const char* name, u32 width, u32 height, u8 channel_count, b8 has_transparency, b8 is_writeable, b8 register_texture, void* internal_data);
+
+/**
+ * @brief Sets the internal data of a texture. Useful for replacing internal data from within the
+ * renderer for wrapped textures, for example.
+ *
+ * @param t A pointer to the texture to be updated.
+ * @param internal_data A pointer to the internal data to be set.
+ * @return True on success; otherwise false.
+ */
+b8 texture_system_set_internal(texture* t, void* internal_data);
+
+/**
+ * @brief Resizes the given texture. May only be done on writeable textures.
+ * Potentially regenerates internal data, if configured to do so.
+ *
+ * @param t A pointer to the texture to be resized.
+ * @param width The new width in pixels.
+ * @param height The new height in pixels.
+ * @param regenerate_internal_data Indicates if the internal data should be regenerated.
+ * @return True on success; otherwise false.
+ */
+b8 texture_system_resize(texture* t, u32 width, u32 height, b8 regenerate_internal_data);
+
+/**
+ * @brief Writes the given data to the provided texture. May only be used on
+ * writeable textures.
+ *
+ * @param t A pointer to the texture to be written to.
+ * @param offset The offset in bytes from the beginning of the data to be written.
+ * @param size The number of bytes to be written.
+ * @param data A pointer to the data to be written.
+ * @return True on success; otherwise false.
+ */
+b8 texture_system_write_data(texture* t, u32 offset, u32 size, void* data);
 
 /**
  * @brief Gets a pointer to the default texture. No reference counting is 
